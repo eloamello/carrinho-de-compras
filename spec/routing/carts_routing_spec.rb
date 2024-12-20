2da2e0cc27618d10ae355c52797761dd9fe829ee
Eloá Mello <eloamello126@gmail.com>
2024-12-20 19:31:08 -0300

    :sparkles: feat: novos testes de rotas

--- spec/routing/carts_routing_spec.rb
+++ spec/routing/carts_routing_spec.rb
@@ -7,11 +7,15 @@ RSpec.describe CartsController, type: :routing do
     end
 
     it 'routes to #create' do
-      pending "#TODO: Escreva um teste para validar a criação de um carrinho #{__FILE__}" 
+      expect(post: '/carts').to route_to('carts#create')
+    end
+
+    it 'routes to #destroy' do
+      expect(delete: '/carts/product_id').to route_to(controller: 'carts', action: 'destroy', product_id: 'product_id')
     end
 
     it 'routes to #add_item via POST' do
-      expect(post: '/cart/add_item').to route_to('carts#add_item')
+      expect(post: '/carts/add_items').to route_to('carts#add_items')
     end
   end
 end 
